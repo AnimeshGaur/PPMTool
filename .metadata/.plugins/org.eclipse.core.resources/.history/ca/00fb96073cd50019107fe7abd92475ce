588b162befccf756108dfddff013d74959bd1761
Animesh Gaur <ani8222@gmail.com>
2019-09-12 17:25:47 +0530

    first

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ca/00fb96073cd50019107fe7abd92475ce
@@ -0,0 +1,5 @@
+package com.ppm.domain;
+
+public class Project {
+
+}
--- PPMTOOL/src/main/java/com/ppm/PpmtoolApplication.java
+++ PPMTOOL/src/main/java/com/ppm/PpmtoolApplication.java
@@ -1,9 +1,14 @@
 package com.ppm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class PpmtoolApplication {
 
 	public static void main(String[] args) {
